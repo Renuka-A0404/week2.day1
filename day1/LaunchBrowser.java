b948c6f7daafb52b42f62bab3831b7a2d140f4e0
Renuka-A0404 <142973549+Renuka-A0404@users.noreply.github.com>
2023-08-28 10:25:05 +0530

    Add files via upload

---
+++ day1/LaunchBrowser.java
@@ -0,0 +1,12 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// setup the path and launch the browser
+		ChromeDriver driver = new ChromeDriver();
+		
+	}
+}
